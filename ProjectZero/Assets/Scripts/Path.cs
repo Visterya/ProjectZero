8c8ac08205c9ed852e046ce27070b8dcb873e6ac
Visterya <yucekara98@gmail.com>
2024-01-24 23:39:11 +0300

    Sling Mechanic Added

---
+++ ProjectZero/Assets/Scripts/Path.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    static GameObject clone = null;
+
+    public static void StartVisualizingPath(GameObject throwable)
+    {
+        throwable.GetComponent<Rigidbody2D>().simulated = false;
+        throwable.GetComponent<CircleCollider2D>().enabled = false;
+
+        clone = Instantiate(throwable, throwable.transform.position, Quaternion.identity);
+        clone.GetComponent<Rigidbody2D>().simulated = true;
+        clone.GetComponent<CircleCollider2D>().enabled = true;
+
+        Physics2D.simulationMode = SimulationMode2D.Script;
+    }
+    public static void VisualizePath(GameObject throwable, Vector3 force)
+    {
+        clone.transform.position = throwable.transform.position;
+
+        Rigidbody2D cloneRigidbody = clone.GetComponent<Rigidbody2D>();
+        cloneRigidbody.velocity = Vector3.zero;
+        cloneRigidbody.AddForce(force);
+
+        List<Vector3> pathPoints = new List<Vector3>();
+        int simulationSteps = 1000;
+        for (int i = 0; i < simulationSteps; i++)
+        {
+            Physics2D.Simulate(Time.fixedDeltaTime);
+            pathPoints.Add(cloneRigidbody.transform.position);
+        }
+
+        LineRenderer linePath = cloneRigidbody.GetComponent<LineRenderer>();
+        linePath.enabled = true;
+        linePath.positionCount = pathPoints.Count;
+        linePath.SetPositions(pathPoints.ToArray());
+
+    }
+    public static void StopVisualizingPath(GameObject throwable)
+    {
+        throwable.GetComponent<CircleCollider2D>().enabled = true;
+        throwable.GetComponent<Rigidbody2D>().simulated = true;
+        Physics2D.simulationMode = SimulationMode2D.FixedUpdate;
+        Destroy(clone);
+    }
+}
--- ProjectZero/Assets/Scripts/Swing.cs
+++ ProjectZero/Assets/Scripts/Swing.cs
@@ -1,18 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Swing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 throwVector;
+
+    [Header("Components")]
+    private Rigidbody2D _rb;
+    private LineRenderer _line;
+
+    private void Awake()
     {
-        
+        _line = this.GetComponent<LineRenderer>();
+        _rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        CalculateThrowVector();
+        Path.StartVisualizingPath(this.gameObject);
+    }
+
+
+    private void OnMouseDrag()
+    {
+        CalculateThrowVector();
+        Path.VisualizePath(this.gameObject, throwVector);
+    }
+    private void SetArrow()
+    {
+        _line.positionCount = 2;
+        _line.SetPosition(0, Vector3.zero);
+        _line.SetPosition(1, throwVector.normalized * 2);
+        _line.enabled = true;
+    }
+    private void CalculateThrowVector()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+
+        Vector2 distance = mousePos - this.transform.position;
+
+        throwVector = -distance.normalized * 500;
+    }
+
+    private void OnMouseUp()
+    {
+        Path.StopVisualizingPath(this.gameObject);
+        Throw();
+    }
+
+    private void Throw()
+    {
+        _rb.AddForce(throwVector);
+    }
+
+    private void RemoveArrow()
+    {
+        _line.enabled = false;
     }
 }
