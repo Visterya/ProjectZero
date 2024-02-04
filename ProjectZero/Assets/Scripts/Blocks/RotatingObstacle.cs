138c59dc8f3bd9d11c3e4abcd31e197c1068252f
Visterya <yucekara98@gmail.com>
2024-02-04 17:47:25 +0300

    Refactoring

---
+++ ProjectZero/Assets/Scripts/Blocks/RotatingObstacle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObstacle : MonoBehaviour
+{
+    [SerializeField, Range(10, 50)] private float rotateSpeed;
+    void Start()
+    {
+        InvokeRepeating("RotateObstacle", 0.1f, 1f);
+    }
+
+    private void RotateObstacle()
+    {
+        transform.Rotate(new Vector3(0, 0, rotateSpeed));
+    }
+}
